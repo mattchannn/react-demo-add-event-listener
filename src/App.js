c236512829a804e12cd8a499daaaeac5f59e444d
Mat Chan <mat.chan@ibm.com>
2022-04-26 15:18:16 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [step, setStep] = useState(0);
+  const updateStep = useCallback(
+    (e) => {
+      console.log(">> updating...");
+      setStep(step + 1);
+    },
+    [step]
+  );
+  useEffect(() => {
+    console.log(">> Adding event listener");
+    const button = document.querySelector(".btn");
+    button.addEventListener("click", updateStep, false);
+    return () => {
+      console.log(">> Removing event listener");
+      button.removeEventListener("click", updateStep);
+    };
+  }, [updateStep]);
   return (
     <div className="App">
       <header className="App-header">
@@ -9,17 +27,16 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{ display: "flex" }}>
+          <button style={{ padding: "0.5rem", marginRight: "1rem" }} className="btn">
+            Increment
+          </button>
+          <div>Step: {step}</div>
+        </div>
       </header>
     </div>
   );
 }
 
 export default App;
+
